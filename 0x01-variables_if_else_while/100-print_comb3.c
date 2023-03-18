c380f139d0a6daaf71c0127bd27e486a9087c89e
foudilboukelal <foudil.boukelal@gmail.com>
2023-03-18 20:58:36 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,7 +15,7 @@ int main(void)
 		{
 			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
-			if (digit1 == 8 && digit2 == 9)
+			if (i == 8 && j == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
