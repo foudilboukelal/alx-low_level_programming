6a6fa28204cd889f9b7f0cc0781c7ee9873334b1
foudilboukelal <foudil.boukelal@gmail.com>
2023-03-18 15:18:47 +0000

    #include <stdio.h> /**  * main - Entry point  *  * Return: Always 0 (Success)  */ int main(void) { char alp[52]=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ; int i;
    for (i = 0 ; i <= 52; i++)
    {
    putchar(alp[i]);
    }
    return (0);
    }''/
    
    q
    q
    '

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[24]="abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0 ; i <= 24; i++)
+	{
+	putchar(alp[i]);
+	}
+	return (0);
+}
